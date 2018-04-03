b9f712b5a22264cea999930a3fb2d0c64f53fe62
k16ds01 <danielle.sarafian16@kzoo.edu>
2018-04-03 17:10:02 -0400

    First commit

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+/*
+ * helloworld.c
+ *
+ *  Created on: Apr 3, 2018
+ *      Author: Danielle Sarafian
+ */
+
+void printGreeting(char greeting)
+{
+	printf("%", greeting);
+}
+
+void main ()
+{
+	printGreeting("Hello world!");
+}
